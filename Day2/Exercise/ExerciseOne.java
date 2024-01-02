76151d2f79188f898e2742e85889d56c09cd1eea
LightAKG <109622706+LightAKG@users.noreply.github.com>
2024-01-02 22:54:42 +0530

    Create ExerciseOne.java

---
+++ Day2/Exercise/ExerciseOne.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ class ExerciseOne {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number you want to print the Sum.");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for(int i=1; i<=n; i=i+1){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
+
+}

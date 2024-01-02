4d9ce3b5d07d0779ef4c420b3ca171717a79a4b5
LightAKG <109622706+LightAKG@users.noreply.github.com>
2024-01-02 22:53:43 +0530

    Create Loops.java

---
+++ Day2/Loops.java
@@ -0,0 +1,12 @@
+class Loops {
+    public static void main(String[] args) {
+        //for loops
+        //for(initialization; condition; updation)
+
+        //if condition is removed then this code will run for infinite times we should remove this immediately
+        for(int counter = 0; counter <= 4; counter = counter+1){
+            System.out.print(counter +" ");
+        }
+    }
+
+}

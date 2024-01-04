1f9da2bff745faa418610f487915ba9d2997d22f
LightAKG <109622706+LightAKG@users.noreply.github.com>
2024-01-05 00:18:48 +0530

    Create Function.java

---
+++ Day4/Function.java
@@ -0,0 +1,18 @@
+ public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = sc.nextInt();
+
+        int sum = calculateSum(a, b);
+
+        System.out.println("The Sum of Numbers is: " + sum);
+    }
+
+    public static int calculateSum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }

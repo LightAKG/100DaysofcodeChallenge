9035568d438644be9e3e51f3d11aa11e4d2620a0
LightAKG <109622706+LightAKG@users.noreply.github.com>
2024-01-06 00:50:52 +0530

    Create Inheritance.java

---
+++ Day5/Inheritance.java
@@ -0,0 +1,18 @@
+public class Inheritance {
+    //taking properties or methods of one class and inheritance
+}
+
+public class Shape{
+    String color; 
+}
+
+class Triangle extends Shape{
+
+}
+class OOPS{
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+        t1.color = "red";
+    }
+    
+}

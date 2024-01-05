3239299ef2194eeaaf9821684b27fcf417a85b69
LightAKG <109622706+LightAKG@users.noreply.github.com>
2024-01-06 00:50:17 +0530

    Create Shape.java

---
+++ Day5/Shape.java
@@ -0,0 +1,17 @@
+ class Shape {
+    public void area(){
+        System.out.println("Displays area");
+    }    
+}
+
+class Triangle extends Shape{
+    public void area(int l, int h){
+        System.out.println(1/2*l*h);
+    }
+}
+
+class EquilateralTriangle extends Triangle{
+    public void area(int l, int h) {
+     System.out.println(1/2*l*h);
+ }   
+}

b327f56f203b27137e2f4fd08a4e35dc3a9ec980
LightAKG <109622706+LightAKG@users.noreply.github.com>
2024-01-08 06:16:49 +0530

    Create Six.java

---
+++ Day7/Six.java
@@ -0,0 +1,11 @@
+
+public class Six {
+    public static void main(String[] args) {
+        
+        int i = 1;
+        do{
+            System.out.println("Enter a number"+i);
+            i++;
+        }while( i > 0);
+    }
+}

d2ada9dd26c7b281ccf4a0bfa9109e8551343008
LightAKG <109622706+LightAKG@users.noreply.github.com>
2024-01-11 23:13:09 +0530

    Create Operators.java

---
+++ Day11/Operators.java
@@ -0,0 +1,108 @@
+public class Operators {
+
+    public static void main(String[] args) {
+        // we call all the mathmatics operators as binary operators because they used to combine two operends with one operator.
+        // Arithmetic operators - binary(+,-,*,/,%)  unary(++, --)
+        
+
+        // pre increment (++a)
+        // a = 10;
+        // b = ++a;
+        // system.out(a); output 11
+        // system.out(b); output 10
+
+        // post increment (a++)
+        // a = 10;
+        // b = a++;
+        // system.out(a); output 11
+        // system.out(b); output 11
+        
+        // pre decrement --a
+        // a = 10;
+        // b = --a;
+        // system.out(a); output 9
+        // system.out(b); output 9
+        
+        //post decrement a--
+        // a = 10;
+        // b = a--;
+        // system.out(a); output 9
+        // system.out(b); output 10
+        
+        
+
+        //Relational Operators
+        // Operators 
+        // ==  equal to
+        // <= smaller then equal to
+        // >= greater then equal to
+        // != not equal to
+
+
+
+        //Logical Operators
+        // operators
+        // (&& Logical and ) if both true => t else false
+        // (|| Logical or) if either true=> t or false
+        // (! logical not) 
+
+
+
+
+        // Number system
+        // binary system base 2 => 1 or 0
+        // decimal system base 10 => 0 to 9
+
+        //binary to decimal 
+        // 1001 = 2^3+ 2^2+ 2^1+ 2^0 = 8 + 0 + 0 + 1 = 9
+
+        // decimal to binary
+        // 12 = 8 4 2 1 
+            //  1 1 0 0 = 1100
+        // octal base 8
+        //hexadecimal base 16
+
+
+
+        // Bitwise Operators
+        // & (Binary AND) 
+        // a = 0101
+        // b = 0110
+        // & = 0100
+        
+        // | (Binary OR)
+        // a = 0101
+        // b = 0110
+        // | =  0111
+
+        // ^ (Binary XOR)
+        // a = 0101
+        // b = 0110
+        // ^ = 0011
+        
+        
+        // ~ (Binary one's complement)
+        // a = 0101
+        // ~ = 1010
+        
+        // b = 0110
+        // ~ = 1001
+        
+        // Binary left shift
+        // << binary left shift
+        // a = 0101
+        // a << 1
+        // a = 1010
+        
+        // Binary right Shift
+        // b>> binary right shift
+        // b = 0110
+        // b >> 1
+        // b= 0011
+
+
+        // Assignment Operators
+        // +=
+
+    }
+}

b2a86a1c6b25982bead7f3c64e073acd6cf241e1
LightAKG <109622706+LightAKG@users.noreply.github.com>
2024-01-19 11:43:32 +0530

    Create Move.java

---
+++ Day15/Move.java
@@ -0,0 +1,25 @@
+public class Move {
+    //move all "x" to the end of the String
+    public static void moveAll(String str, int idx, int count, String newString){
+        if(idx == str.length()){
+            for(int i=0; i<count; i++){
+                newString += 'x';
+            }
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        if(currChar == 'x'){
+            count++;
+            moveAll(str, idx+1, count, newString);
+        } else {
+            newString += currChar; //newString = newString + currChar;
+            moveAll(str, idx+1, count, newString);
+
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
